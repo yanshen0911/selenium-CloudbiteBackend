b934200ccda0160c61b9b5574d465acf864279ec
Keith Chu <keith.chu@qubeapps.com>
2025-01-13 15:54:47 +0800

    fix: selenium driver and browser path

--- SeleniumTests/Driver/DriverFactory.cs
+++ SeleniumTests/Driver/DriverFactory.cs
@@ -9,7 +9,17 @@ namespace ERPPlus.SeleniumTests.Drivers
         {
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--start-maximized");
-            return new ChromeDriver(options);
+
+            // please make sure you have the EXACT chrome driver version downloaded and extracted
+            // this one is manual downlaod at https://developer.chrome.com/docs/chromedriver/downloads
+            // you need to map it with your current chrome version, and need to change from time to time
+            
+            string _ChromeBrowserPath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+            string _ChromeDriverPath = @"C:\Program Files\Google\Chrome\Application\131.0.6778.265\chromedriver-win64";
+
+            options.BinaryLocation = _ChromeBrowserPath; 
+            IWebDriver driver = new ChromeDriver(_ChromeDriverPath, options);
+            return driver;
         }
     }
 }
